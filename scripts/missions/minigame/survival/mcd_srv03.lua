53f59c670ddfd6165381fb8d375197ee2e37af3b
SoapyMan <ishurumov@gmail.com>
2025-01-14 12:17:42 +0500

    - make survival missions use SurvivalGame

--- scripts/missions/minigame/survival/mcd_srv03.lua
+++ scripts/missions/minigame/survival/mcd_srv03.lua
@@ -16,19 +16,6 @@ SetMusicName("la_day")
 ----------------------------------------------------------------------------------------------
 
 MISSION.Init = function()
-
-	MISSION.Data = {}
-	
-	-- adjust cops
-	MISSION.Settings.EnableCops = true
-	MISSION.Settings.EnableRoadBlock = false
-	MISSION.Settings.CopAccelerationScale = 2.3
-	MISSION.Settings.CopMaxSpeed = 1000;
-	MISSION.Settings.MinCops = 10
-	MISSION.Settings.MaxCops = 12
-	MISSION.Settings.MaxCopDamage = 100000;
-	
-	
 	-- car name		maxdamage	pos ang
 	local playerCar = gameses:CreateCar("mcd_miamidef", CAR_TYPE_NORMAL)
 	MISSION.playerCar = playerCar
@@ -41,57 +28,5 @@ MISSION.Init = function()
 
 	gameses:SetPlayerCar( playerCar )
 	
-	missionmanager:ShowTime( true ) -- enable time display
-	
-	gameHUD:ShowScreenMessage("#SURVIVAL_TITLE", 3.5)
-	
-	-- this is temporary to spawn cops
-	MISSION.Settings.CopRespawnInterval = 0
-	MISSION.Settings.CopWantedRespawnInterval2 = 0
-	
-	-- here we start
-	missionmanager:SetRefreshFunc( function(dt)
-	
-		-- do first game update frame to spawn cops and adjust params
-		felonySystem:MakePursued( playerCar )
-		playerCar:SetFelony(1.0)
-
-	
-		-- continue with standard update routine
-		missionmanager:SetRefreshFunc(MISSION.Update)
-		
-		return false
-	end	)
-end
-
---------------------------------------------------------------------------------
-
-function MISSION.OnFailed()
-	gameses:SetLeadCar( MISSION.playerCar )
-	MISSION.playerCar:Lock(true)
-end
-
--- main mission update
-function MISSION.Update( delta )
-
-	local playerCar = MISSION.playerCar
-
-	UpdateCops( playerCar, delta )
-	felonySystem:MakePursued( playerCar )
-
-	-- check player vehicle is wrecked
-	if CheckVehicleIsWrecked( playerCar, MISSION.Data, delta ) then
-		gameHUD:ShowAlert("#WRECKED_VEHICLE_MESSAGE", 3.5, HUD_ALERT_DANGER)
-		
-		MISSION.OnFailed()
-		
-		missionmanager:SetRefreshFunc( function() 
-			return false 
-		end ) 
-
-		gameses:SignalMissionStatus( MIS_STATUS_FAILED, 4.0 )
-		return false
-	end
-	
-	return true
+	SurvivalGame.Init( 8 )
 end
